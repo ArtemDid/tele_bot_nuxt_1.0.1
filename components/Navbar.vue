edf76d897fb2f5adfd823320997e11f89f92dfda
ArtemDid <temadidenko27@gmail.com>
2021-06-04 13:27:50 +0300

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar color="purple" dark>
+    <v-spacer></v-spacer>
+    <span class="subheading">My Home</span>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text> News </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text> Blog </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text> Music </v-btn>
+
+      <v-btn text> Music </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
